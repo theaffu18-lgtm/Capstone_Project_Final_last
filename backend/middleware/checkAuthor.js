e5b491b12d83e85bdf6c7d97f3215138fe227aac
Harish <24eg505k03@aanurag.edu.in>
2026-05-16 13:44:04 +0530

    fixed file casing1

--- backend/middleware/checkAuthor.js
+++ backend/middleware/checkAuthor.js
@@ -3,8 +3,10 @@ import { UserTypeModel } from "../models/userModel.js";
 export const checkAuthor = async (req, res, next) => {
   //get author id
   let aid = req.body?.author || req.params?.authorId;
+  console.log("Author ID from request:", aid);
   //verify author
   let author = await UserTypeModel.findById(aid);
+  console.log("Author found:", author);
   //if author not found
   if (!author ) {
     return res.status(401).json({ message: "Invalid Author" });
